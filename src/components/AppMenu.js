c4cf5f886cb96abb0e8fe093878e39307af41d5c
mmj-ssb <mmj@ssb.no>
2020-12-15 12:05:08 +0100

    Forgot new logo

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { Dropdown, Header, Image, Menu, Sticky } from 'semantic-ui-react'
-import { LANGUAGE, SSB_COLORS, ssb_logo_rgb } from '@statisticsnorway/dapla-js-utilities'
+import { LANGUAGE, SSB_COLORS, ssb_logo_no_text_rgb, ssb_logo_rgb } from '@statisticsnorway/dapla-js-utilities'
 
 import { LanguageContext } from '../context/AppContext'
 import { UI } from '../enums'
@@ -23,7 +23,7 @@ function AppMenu ({ setSettingsOpen, context }) {
         }}
       >
         <Menu.Item>
-          <Image size={menuIsStuck ? 'mini' : 'medium'} src={menuIsStuck ? ssb_logo_rgb : ssb_logo_rgb} />
+          <Image size={menuIsStuck ? 'mini' : 'medium'} src={menuIsStuck ? ssb_logo_no_text_rgb : ssb_logo_rgb} />
         </Menu.Item>
         <Menu.Item>
           <Header size={menuIsStuck ? 'medium' : 'huge'} content={UI.HEADER[language]} />
